531bfee98752f2d3f56b7ee378b730c430ece020
danielkmx <danielbezerrakmxgmail.com>
2018-07-21 14:14:51 -0300

    Commit inicial estrutura de buscas no KoboToolbox

--- Services/KoboApiService.py
+++ Services/KoboApiService.py
@@ -1,28 +1,66 @@
 import xlrd
 import requests
 import json
+import os
 
 
 def imprimir_lista_formularios(u_r_l, u_s_e_r, p_a_s_s_w_o_r_d):
-    r = requests.get(u_r_l, auth=(u_s_e_r, p_a_s_s_w_o_r_d))
+    r = requests.get('https://' + u_r_l + '/api/v1/data', auth=(u_s_e_r, p_a_s_s_w_o_r_d))
     lista = list()
     lista_content = json.loads(r.content)
-    for elemento in lista_content:
-        lista.append(elemento['url'])
-    return lista
+    return lista_content
 
+def retorna_respostas_com_labels(u_r_l, i_d, u_s_e_r, p_a_s_s_w_o_r_d):
+    formulario = retorna_lista_com_labels(u_r_l, i_d, u_s_e_r, p_a_s_s_w_o_r_d)
+    if u_r_l == 'kobocat.docker.kobo.techo.org':
+        u_r_l = 'koboform.docker.kobo.techo.org'
+    if u_r_l == 'kc.humanitarianresponse.info':
+            u_r_l = 'kobo.humanitarianresponse.info'
+    req_url = 'https://' + u_r_l + '/assets/' + formulario[1] + '.json'
+    req = requests.get(req_url, auth=(u_s_e_r, p_a_s_s_w_o_r_d))
+    lista_content = json.loads(req.content)
+    lista_perguntas_labels = dict()
+    for item in lista_content['content']['survey']:
+        if 'select_from_list_name' in item:
+            for pergunta in lista_content['content']['choices']:
+                    if pergunta['list_name'] in item['select_from_list_name']:
+                        lista_perguntas_labels[item['label'][0]] = pergunta['label'][0]
+    for resposta in formulario[0]:
+        for key, value in lista_perguntas_labels.items():
+            if key in resposta:
+                resposta[key] = value
+    return formulario
 
-def imprimir_dados_formularios(lista_urls, u_s_e_r, p_a_s_s_w_o_r_d):
-    lista = list()
-    for formulario_url in lista_urls:
-        r = requests.get(formulario_url, auth=(u_s_e_r, p_a_s_s_w_o_r_d))
-        formulario_json = json.loads(r.content)
-        lista.append(formulario_json)
-    return lista
+def retorna_key_do_formulario(u_r_l, id, u_s_e_r, p_a_s_s_w_o_r_d):
+    req = requests.get(u_r_l, auth=(u_s_e_r, p_a_s_s_w_o_r_d))
+    lista_content = json.loads(req.content)
+    for item in lista_content:
+        if item['id'] == id:
+            return item['id_string']
 
+def retorna_lista_com_labels(u_r_l, i_d, u_s_e_r, p_a_s_s_w_o_r_d):
+    id_string = retorna_key_do_formulario('https://' + u_r_l + '/api/v1/data', i_d, u_s_e_r, p_a_s_s_w_o_r_d)
+    formulario = importar_xls_para_lista_filtrada('https://' + u_r_l + '/api/v1/data/' + str(i_d) + '.xls', u_s_e_r, p_a_s_s_w_o_r_d)
+    if u_r_l == 'kobocat.docker.kobo.techo.org':
+        u_r_l = 'koboform.docker.kobo.techo.org'
+    if u_r_l == 'kc.humanitarianresponse.info':
+        u_r_l = 'kobo.humanitarianresponse.info'
+    req = requests.get('https://' + u_r_l + '/assets/' + id_string + '.json', auth=(u_s_e_r, p_a_s_s_w_o_r_d))
+    lista_content = json.loads(req.content)
+    labels_dict = dict()
+    for item in lista_content['content']['survey']:
+        if 'label' in item:
+            labels_dict[item['$autoname']] = item['label']
+    for enquete in formulario:
+        for key, value in labels_dict.items():
+            if key in enquete:
+                enquete[value[0]] = enquete.pop(key)
+    retorno = [formulario, id_string]
+    return retorno
 
 def importar_xls_para_lista_filtrada(u_r_l, u_s_e_r, p_a_s_s_w_o_r_d):
     req = requests.get(u_r_l, auth=(u_s_e_r, p_a_s_s_w_o_r_d))
+    file = "formulario.xls"
     with open('formulario.xls', 'wb') as output:
         output.write(req.content)
     workbook = xlrd.open_workbook('formulario.xls')
@@ -44,6 +82,9 @@ def importar_xls_para_lista_filtrada(u_r_l, u_s_e_r, p_a_s_s_w_o_r_d):
                     record[worksheet.cell(0, y).value] = worksheet.cell(x, y).value
         if record:
             table.append(record)
+    try:
+        os.remove(file)
+    except OSError as e:
+        print("Error %s - %s" % (e.filename, e.strerror))
     return table
 
-print(teste)
---
+++ Services/MongoRepository.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify
+from Services import KoboApiService as kapi
+from flask_restplus import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+app.config['JSON_AS_ASCII'] = False
+
+ns = api.namespace('Formularios', description='Operações para filtrar dados das enquetes registradas no KoboToolbox')
+
+@ns.route('/formulario/<string:url>/<int:i_d>/<string:username>/<string:password>')
+@ns.response(404,'Página não encontrada, o endereço inserido deve estar incorreto!')
+class Formulario(Resource):
+    def get(self, url, i_d, username, password):
+        """ Retorna todas enquetes preenchidas
+
+            Use a rota /formularios para conseguir o ID das enquetes
+
+
+
+                 Urls do kobo legado:
+                kc.humanitarianresponse.info
+                koboform.docker.kobo.techo.org
+
+                """
+
+        return jsonify(kapi.retorna_respostas_com_labels(url, i_d, username, password))
+
+@ns.route('/formularios/<string:url>/<string:username>/<string:password>')
+@ns.response(404,'Página não encontrada, o endereço inserido deve estar incorreto!')
+class Formularios(Resource):
+    def get(self, url, username, password):
+        """
+        Retorna Ids e nomes de todos formularios
+
+             Urls do kobo legado:
+
+
+                kc.humanitarianresponse.info
+                koboform.docker.kobo.techo.org
+            """
+        return jsonify(kapi.imprimir_lista_formularios(url, username, password))
+if __name__ == '__main__':
+    app.run(debug=True)
+
